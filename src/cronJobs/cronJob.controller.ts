db8a6f82dc2dd85fe88bcaa464652689772c6d4f
dk <dkbalwan0101@gmail.com>
2025-03-05 12:43:35 +0530

    second

--- src/cronJobs/cronJob.controller.ts
+++ src/cronJobs/cronJob.controller.ts
@@ -4,6 +4,7 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { CronJobDto } from "src/dto/cronjob.dto";
 import { cronJobService } from "./cronJob.service";
+import { promises } from "dns";
 
 
 @Controller('cronJob')
@@ -16,33 +17,56 @@ export class CronJobController{
     // to create cronjobs
     @Post('create')
     async Create(@Body() job:CronJobDto){
-        const createdJob = new this.CronJobModel(job)
-        return await createdJob.save()
+        try {
+            const createdJob = new this.CronJobModel(job)
+            return await createdJob.save()
+        } catch (error) {
+           console.log('failed to create job',error) 
+        }
     }
 
     // to get all cron jobs
     @Get('all')
     async FindAll(){
-        return await this.CronJobModel.find({})
+        try {
+            return await this.CronJobModel.find({})
+        } catch (error) {
+            console.log('failed to get jobs',error);
+            
+        }
     }
 
     // to get one cron job by id
     @Get(':id')
     async FindbyId(@Param('id') id: any){
-        console.log(id);
-        return await this.CronJobModel.findById(id).exec()
+        try {
+            console.log(id);
+            return await this.CronJobModel.findById(id).exec()
+        } catch (error) {
+            console.log('failed to get job by id',error);
+            
+        }
     }
 
     // to update cron job by id
     @Put('update/:id')
-    async Update(@Param('id') id: any, @Body() updateJob: CronJobDto) {
-        return  await this.CronJobModel.findByIdAndUpdate(id,updateJob)
+    async Update(@Param('id') id: any, @Body() updateJob: CronJobDto){
+        try {
+            return await this.CronJobModel.findByIdAndUpdate(id,updateJob).exec()
+        } catch (error) {
+            console.log('failed to update',error);
+        }
     }
 
     // to delete cron job
     @Delete('delete/:id')
-    async Remove(@Param('id') id: any) {
-        return await this.CronJobModel.findByIdAndDelete(id).exec()
+    async Remove(@Param('id') id: any){
+        try {
+            return await this.CronJobModel.findByIdAndDelete(id).exec()
+        } catch (error) {
+            console.log('failed to delete',error);
+            
+        }
     }
 
 }
